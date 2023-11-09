606a578ce0f1507751d11cc2e2a10afbac64c928
amanavi75 <amanavi75@gmail.com>
2023-11-10 01:28:22 +0530

    started the server

--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+         console.log(`server is running at port:${process.nextTick.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongo db  connection failed " , err);
+})
+
 
 
 
