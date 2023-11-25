bd78e20dab7c4c443721ab10a23dd04190d286d2
amanavi75 <amanavi75@gmail.com>
2023-11-26 01:08:24 +0530

    configured the configurations and important middlewares

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
-import express from "express"
+import express, { urlencoded } from "express"
 import cors from "cors"
-import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser"; 
+//* cookieParser is  used to access the browser cookie of server and also use to set it 
 
 const app = express();
 
-app.use(cors({
+app.use(cors({     
     origin:process.env.CORS_ORIGIN,
     credentials:true
-})); //* using cors middleware
+}));
+//* using cors middleware ( cross origin resourse sharing)
+//* app.use is used for middlewares and configuaration
+
+app.use(express.json({limit:"16kb"}))  
+//used to configure json
+// in earlier time we have to use to body parser 
+
+app.use(express.urlencoded({extended:true, limit:"16kb"}))  
+// extended is true means we can give objects inside the objects 
+
+app.use(cookieParser())
+//* cookieParser is  used to access the browser cookie of server and also use to set it 
 
 
+app.use(express.static("public"))  // used to acess the public folder or files from the local 
 
 export {app}
