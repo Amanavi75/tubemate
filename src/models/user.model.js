edd76583c5ad7e3762a433cbf89998505386e99c
amanavi75 <amanavi75@gmail.com>
2023-11-27 02:00:41 +0530

    done with user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -20,17 +20,36 @@ const userSchema = new Schema (
       fullname: {
         type:String,
         required: true,
-        unique: true,
-        lowecase: true,
         trim : true,
+        index: true
       },
+      avatar:{
+        type: String, // se will use cloudnary 
+        required:true,
+      },
+      coverImage:{
+        type: String, // se will use cloudnary 
+      },
+      watchHistory:[
+             {
+                type: Schema.Types.ObjectId,
+                ref:"Video",
 
+             }
+      ],
+      password: {
+        type: String,
+        required: [true,'password is required']
+      },
+      refreshToken: {
+        type: String
+      },
+      
+   },
+   {
+    timestamps: true,
    }
 )
-
-
-
-
 
 
 
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,9 +1,51 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoschema = new Schema (
+    {
+       videoFile :{
+        type: String, //cloudnary
+        required:true 
+       },
+      thumbnail :{
+        type: String, //cloudnary
+        required:true 
+       },
+       title :{
+        type: String, //cloudnary
+        required:true 
+       },
+       description :{
+        type: String,
+        required:true 
+       },
+        duration:{
+        type: Number, // cloudnary  
+        required:true 
+       },
+       views: {
+        type:Number,
+        default: 0,
+       },
+       isPublished: {
+        type: Boolean,
+        default:true,
+       },
+       owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+       }
+    },
+    {
+        timestamps:true,
+    }
+)
 
-const videoschema = new Schema ({
 
-})
+videoschema.plugin(mongooseAggregatePaginate)  //plugin for mongooseAggregatePaginate
 
 
+
+// aggregation queries in mongodb 
+//*we will  mongoose-aggregate-paginate-v2 package for writting thses types of quereis 
 
 export const Video = mongoose.model("Video",videoschema);
