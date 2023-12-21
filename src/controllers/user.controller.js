791d2b441be3a93984c3b4db7ff7bc6cda354a3c
amanavi75 <amanavi75@gmail.com>
2023-12-21 12:19:22 +0530

    added validation for the empty field and som bugs fixed in the routes regaridng  multer package

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,35 @@
 import {asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
 
 const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message: "ok"
-    })
+    //* steps 
+   // get user details from frontend 
+   //validations- the field shoudn't be empty
+   //check if user already exists: username or email 
+   // check for thr images , check for the avatar ... (upload it on cloudinary server)
+   // create user Object - create entry in db 
+   // remove password and refresh token field from response 
+   //check for the user creation 
+   //return res
+
+   const {fullName,email,userName,password} = req.body // step 1
+
+   //if(fullName===""){
+    //throw new ApiError(400,"full Name is required")
+   //} common way to check validation 
+
+   // for optimization we will use  .some() method 
+
+   if(
+    [fullName,email,userName,password].some((field)=>
+        field?.trim()==="") // will check there shouldn't be any empty field
+    ){
+       throw new ApiError(400,"all fields are required")
+    }
+   
+   
 })
+
 
 
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,24 @@
 import { Router } from "express";
 import {registerUser} from "../controllers/user.controller.js"
+ import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router()
 
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+      {
+        name: "avatar",
+        maxCount:1
+      },
+      {
+        name:"coverImage",
+        maxCount:1
+      }
+    ]),
+    registerUser)
 
 
 export default router;
+
+//adding  upload.field middleware so that we will able to upload file during registration 
