bd2a9674bf665fbbbad9af20603ea16d4ffe9028
amanavi75 <amanavi75@gmail.com>
2024-01-19 10:14:07 +0530

    done with all the controllers of tweet

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -55,12 +55,74 @@ const getUserTweets = asyncHandler(async (req, res) => {
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
-     
+     const {content} = req.body;
+     const {tweetId} = req.body;
+
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(404, " unable to find the tweetId or userId")
+    }
+
+    const tweet = await Tweet.findById(tweetId);
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found");
+    }
+
+
+    if(tweet.owner?.toString()!==req.user._id){
+        throw new ApiError(400, " only woner of the tweet can update or edit the tweet")
+    }
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+        tweetId,{
+            $set:{
+                content:content
+            }
+        },
+        {new:true}
+    )
+
+    if(!updatedTweet){
+        throw new ApiError(400,"some error in updating the tweet")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,updatedTweet,"tweet Updated successfully"));
+
+
 
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const {tweetId} = req.body
+
+    if(!isValidObjectId(tweetId)){
+        throw new  ApiError(400,"tweetId is not valid")
+    }
+
+    const tweet = await Tweet.findById(tweetId);
+
+    if(!tweet){
+        throw new ApiError(404,"tweet not found")
+    }
+
+    if(tweet.owner?.toString()!==req.user._id){
+        throw new ApiError(400, "only owner can delete his tweet")
+    }
+
+    const deleteTweet = await Tweet.findByIdAndDelete(tweetId)
+
+    if(!deleteTweet){
+        throw new ApiError(400, "error in deleting the tweet")
+
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,"tweet deleted successfully"))
+
 })
 
 export {
