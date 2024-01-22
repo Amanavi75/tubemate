182f6be4989a5657be08dd32eca30850518d2a59
amanavi75 <amanavi75@gmail.com>
2024-01-22 10:04:46 +0530

    done with the updateComment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -44,6 +44,41 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId} = req.params;
+    const {content} = req.body;
+
+    if(!content) {
+        throw new ApiError(400,"content is required")
+
+    }
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found");
+    }
+
+    if(req.user?._id.toString()!=comment.owner.toString()){
+        throw new ApiError(400, "only owner can update his comment")
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+        comment?.id,
+        {
+            $set:{
+                content:content
+            }
+          
+        },
+        {new:true}
+    )
+
+    if(!updatedComment){
+        throw new ApiError(400,"unable to update or edit the comment")
+    }
+    
+    return res
+    .status(200)
+    .json(new ApiResponse(200,updatedComment,"comment edited successfully"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
