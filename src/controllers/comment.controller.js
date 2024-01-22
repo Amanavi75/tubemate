cf3028d0fe5cd48864773c99d888813d94e65efe
amanavi75 <amanavi75@gmail.com>
2024-01-22 10:12:04 +0530

    fixed bug ,written deletecomment controller only getVideoComments is left

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -57,7 +57,7 @@ const updateComment = asyncHandler(async (req, res) => {
         throw new ApiError(404, "Comment not found");
     }
 
-    if(req.user?._id.toString()!=comment.owner.toString()){
+    if(comment.owner.toString()!=req.user?._id.toString()){     
         throw new ApiError(400, "only owner can update his comment")
     }
 
@@ -83,6 +83,23 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params;
+   
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found");
+    }
+
+    if(comment.owner.toString()!=req.user?._id.toString()){     
+        throw new ApiError(400, "only comment  owner can delete  his comment")
+    }
+
+    await Comment.findByIdAndDelete(commentId)
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Comment deleted successfully"));
 })
 
 export {
