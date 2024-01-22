f06270fe6046acd515a7d845aac78dfe4d1eba87
amanavi75 <amanavi75@gmail.com>
2024-01-22 09:51:59 +0530

    started with the comment controlle

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,8 +1,9 @@
-import mongoose from "mongoose"
+import mongoose, { isValidObjectId } from "mongoose"
 import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
@@ -13,6 +14,32 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {videoId} = req.params;
+    const {content} = req.body ;
+
+   if(!content){
+    throw new ApiError("content is required")
+   }
+    const video = await Video.findById(videoId);
+
+    if(!video){
+        throw new ApiError("unable to  find video")
+    }
+
+    const comment = await Comment.create({
+        content:content,
+        video:videoId,
+        owner:req.user?._id
+    })
+
+    if(!comment){
+        throw new ApiError(400,"failed to add comment Please try again")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,comment,"comment added successfully"))
+
 })
 
 const updateComment = asyncHandler(async (req, res) => {
