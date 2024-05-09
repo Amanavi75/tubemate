b4203538f5cac5dedff9dc3714edfc1cba5b4c57
amanavi75 <amanavi75@gmail.com>
2024-05-09 11:15:15 +0530

    started with the comment routes

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -3,3 +3,12 @@ import {getVideoComments,
     addComment, 
     updateComment,
      deleteComment} from "../controllers/comment.controller.js"
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import router from "./user.routes.js";
+
+const router = Router(); // creating instance for using the router object 
+
+
+     router.route("/comments").get( verifyJWT, getVideoComments);
+     router.route("/addComment").post(verifyJWT, addComment);
